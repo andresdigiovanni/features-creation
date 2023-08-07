d51d9535ffd7a295aa721dddc00d780861c2037a
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2023-08-07 12:41:34 +0200

    added automatic binning algorithms

--- features_creation/transformations/bins_transformations.py
+++ features_creation/transformations/bins_transformations.py
@@ -1,20 +1,34 @@
+import numpy as np
 import pandas as pd
+from scipy.optimize import minimize_scalar
+from scipy.stats import sem, skew
 
 from .base_transformations import Transformations
 
 
 class BinsTransformations(Transformations):
     _ops = ["cut", "qcut"]
-    _bins = [2, 3, 5, 8, 13]
+    _bins = [2, 3, 5, 8, 13]  # default bins to check
 
     def generate_transformations(self, df, columns):
         transformations = {}
 
         for i_1 in range(len(columns)):
+            n_bins_automatic = self._calculate_binning_algorithms_number_of_bins(
+                df[columns[i_1]]
+            )
+
             for op in self._ops:
-                for bins in self._bins:
-                    col_name = f"{columns[i_1]}__{op}_{bins}"
-                    intervals = self._calculate_intervals(df[columns[i_1]], op, bins)
+                n_bins_optimizated = [
+                    self._calculate_optimizated_number_of_bins(df[columns[i_1]], op)
+                ]
+
+                bins = list(set(self._bins + n_bins_automatic + n_bins_optimizated))
+                bins = [x for x in bins if x > 1]
+
+                for n_bins in bins:
+                    col_name = f"{columns[i_1]}__{op}_{n_bins}"
+                    intervals = self._calculate_intervals(df[columns[i_1]], op, n_bins)
 
                     transformations[col_name] = {
                         "column": columns[i_1],
@@ -24,6 +38,68 @@ class BinsTransformations(Transformations):
                     }
 
         return transformations
+
+    def _calculate_binning_algorithms_number_of_bins(self, column):
+        return [
+            self._calculate_doane_number_of_bins(column),
+            self._calculate_freedman_diaconis_rule_number_of_bins(column),
+            self._calculate_rice_rule_number_of_bins(column),
+            self._calculate_scotts_normal_reference_rule_number_of_bins(column),
+            self._calculate_square_root_number_of_bins(column),
+            self._calculate_sturges_rule_number_of_bins(column),
+        ]
+
+    def _calculate_doane_number_of_bins(self, column):
+        g1 = skew(column)
+        sigma_g1 = sem(column)
+
+        n_bins = 1 + np.log2(len(column)) + np.log2(1 + abs(g1) / sigma_g1)
+        return int(n_bins)
+
+    def _calculate_freedman_diaconis_rule_number_of_bins(self, column):
+        iqr = np.percentile(column, 75) - np.percentile(column, 25)
+        bin_width_fd = 2 * iqr / np.cbrt(len(column))
+        n_bins = (column.max() - column.min()) / bin_width_fd
+
+        return int(n_bins)
+
+    def _calculate_rice_rule_number_of_bins(self, column):
+        n_bins = round(np.cbrt(len(column)))
+        return int(n_bins)
+
+    def _calculate_scotts_normal_reference_rule_number_of_bins(self, column):
+        n_bins = np.ceil(3.5 * np.std(column) * len(column) ** (-1 / 3))
+        return int(n_bins)
+
+    def _calculate_square_root_number_of_bins(self, column):
+        n_bins = np.ceil(np.sqrt(len(column)))
+        return int(n_bins)
+
+    def _calculate_sturges_rule_number_of_bins(self, column):
+        n_bins = 1 + np.log2(len(column))
+        return int(n_bins)
+
+    def _calculate_optimizated_number_of_bins(self, column, op):
+        min_bins = 2
+        max_bins = 20
+
+        result = minimize_scalar(
+            lambda num_bins: self._variance_of_bins(column, op, int(num_bins)),
+            bounds=(min_bins, max_bins),
+            method="bounded",
+        )
+
+        return int(result.x)
+
+    def _variance_of_bins(self, column, op, num_bins):
+        if op == "cut":
+            bins = pd.cut(column, bins=num_bins)
+
+        elif op == "qcut":
+            bins = pd.qcut(column.rank(method="first"), q=num_bins)
+
+        bin_means = column.groupby(bins).mean()
+        return np.var(bin_means)
 
     def _calculate_intervals(self, column, op, bins):
         if op == "cut":
